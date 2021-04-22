a83e36b7e7dafa193989e09bae9dd936fbd01eab
bdey05 <73259986+bdey05@users.noreply.github.com>
2021-04-22 00:33:22 -0400

    Add files via upload

--- Button.java
+++ Button.java
@@ -1,7 +1,14 @@
+package TicTacToe;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 
 
-public class Button {
+public class Button extends Main {
 
 	private int index1;
 	private int index2;
@@ -12,7 +19,18 @@ public class Button {
 		index1 = i;
 		index2 = j;
 		button = new JButton();
+		button.setBackground(Color.WHITE);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+	            button.setText(playerPick);
+	            button.setForeground(Color.BLUE);
+	            button.setFont(new Font("Purisa", Font.BOLD,button.getHeight()));
+	         }
+		});
 	}
+	
+	
+	
 	
 	public JButton getButton(){return button;}
 	
--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+package TicTacToe;
+
 import javax.swing.*; 
 
 public class Main {
@@ -6,12 +8,17 @@ public class Main {
 	private static int opponent; 
 	private static String Player2; 
 	private static Board board; 
+	private static int order;
+	private static int choice;
+	private static String whoFirst;
+	public static String playerPick;
+	public static String player2Pick;
+	private static String cpuPick;
 	
 	public static void main(String[] args) {
 		
 		//Ask the user for name
-		Player1 = JOptionPane.showInputDialog("Enter the name of Player 1: ");
-		
+	
 		/* Ask user if the opponent is another player or a computer 
 		 * 
 		 * opponent = 0 if User pick Computer
@@ -19,12 +26,36 @@ public class Main {
 		 * 
 		 */
 		String[] opponentOption = {"Computer", "Player"}; 
-		opponent = JOptionPane.showOptionDialog(null,"Who do you want to play against?: ", "Choose your opponent", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, opponentOption, null);
+		opponent = JOptionPane.showOptionDialog(null,"Who do you want to play against?", "Choose your opponent", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, opponentOption, null);
+		
+		Player1 = JOptionPane.showInputDialog("Enter the name of Player 1: ");
 		
 		
 		// If the opponent is another player, ask user the name of that player
 		if (opponent == 1){
 			Player2 = JOptionPane.showInputDialog("Enter the name of Player 2: ");
+			String[]choices = {"X", "O"};
+			choice = JOptionPane.showOptionDialog(null, Player1 + " ,do you want to be X or O?", "Choose", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, choices, null); 
+			if (choice == 0) {
+				JOptionPane.showMessageDialog(null, Player1 + " is X and " + Player2 + " is O");
+				playerPick = "X";
+				player2Pick = "O";
+			}
+			if (choice == 1) {
+				JOptionPane.showMessageDialog(null, Player1 + " is O and " + Player2 + " is X");
+				playerPick = "O";
+				player2Pick = "X";
+			}
+			String[] orderOption = {Player1, Player2}; 
+			order = JOptionPane.showOptionDialog(null,"Who do you want to have the first move?: ", "Choose who gets the first move", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, orderOption, null); 
+			 if(order == 0) {
+				 whoFirst = Player1;
+				 JOptionPane.showMessageDialog(null, Player1 + " goes first!");
+			 }
+			 if(order == 1) {
+				 whoFirst = Player2;
+				 JOptionPane.showMessageDialog(null, Player2 + " goes first!");
+			 }
 		}
 			
 		/* If the opponent is a computer, ask user who gets to play first
@@ -35,8 +66,27 @@ public class Main {
 		 */
 		if (opponent == 0){
 			String[] orderOption = {"Computer", "Player"}; 
-			int order = JOptionPane.showOptionDialog(null,"Who do you want to have the first move?: ", "Choose who gets the first move", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, orderOption, null); 
+			order = JOptionPane.showOptionDialog(null,"Who do you want to have the first move?: ", "Choose who gets the first move", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, orderOption, null);
+			 if(order == 0) {
+				 whoFirst = "Computer";
+				 JOptionPane.showMessageDialog(null, "Computer goes first!");
+			 }
+			 if(order == 1) {
+				 whoFirst = Player1;
+				 JOptionPane.showMessageDialog(null, Player1 + " goes first!");
+			 }
+			String[]choices = {"X", "O"};
+			choice = JOptionPane.showOptionDialog(null, Player1 + " ,do you want to be X or O?", "Choose", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, choices, null); 
+			if (choice == 0) {
+				JOptionPane.showMessageDialog(null, Player1 + " is X and " + "the Computer is O");
+			}
+			if (choice == 1) {
+				JOptionPane.showMessageDialog(null, Player1 + " is O and " + "the Computer is X");
+			}
 		}
+		
+		
+		
 		
 		// Create a TicTacToe Board
 		board = new Board(); 	
